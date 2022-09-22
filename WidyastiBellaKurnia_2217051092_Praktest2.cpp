e19322bd63075c828d59e91a6205901555e00daf
Widyasti Bella Kurnia <112605699+widyastibella@users.noreply.github.com>
2022-09-22 17:46:18 +0700

    Add files via upload

---
+++ WidyastiBellaKurnia_2217051092_Praktest2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main (){
+	int angka;
+	cin>>angka;
+	if (angka==1){
+		cout<<"satu\n";
+	}else if (angka==2){
+		cout<<"dua";
+	}else if (angka==3){
+		cout<<"tiga";
+	}else{
+		cout<<"bilangan lebih besar dari tiga";
+	}
+	return 100;
+	
+}
